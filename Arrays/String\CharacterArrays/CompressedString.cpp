4036af25d3213530ca2ec96c113bb4647edf0af1
Kiran Yadav <kiranyadav9551@gmail.com>
2022-03-14 15:01:36 +0530

    updated

--- Arrays/String\CharacterArrays/CompressedString.cpp
+++ Arrays/String\CharacterArrays/CompressedString.cpp
@@ -1,25 +1,64 @@
 # include <iostream>
 # include <vector>
 using namespace std;
+int CompressedString(vector<char>& chars){
+  int i=0;
+  int ansIndex=0;
+  int n=chars.size();
+  while(i<n){
+    int j=i+1;
+    while(j<n && chars[i]==chars[j]){
+      j++;
+    }
+    //yaha kab aaoge - jab pura vector traverse kar liya ho
+    // ya phir different character encounter kiya ho
+    //old character store kar lo
+    chars[ansIndex++]=chars[i];
+    int count =j-i;
+    if(count>1){
+      string cnt=to_string(count);
+      for(char ch : chars){
+         chars[ansIndex++]=ch;
+      }
+    }
+    i=j;
+  }
+  for(int i=0; i<chars.size(); i++){
+       cout<<chars[i]<<" ";
+   }
+  return ansIndex;
+}
 int CompressedStrings(vector<char>& chars){
     int start=0;
     int end=start;
     vector<char> NewChar;
     int count=0;
+    cout<<" End outside loop "<<end<<endl;
     while(end<=(chars.size()-1)){
+
       if(chars.at(start)==chars.at(end)){
+         cout<<" End inside  loop  if part "<<chars[start]<<'\t'<<chars[end]<<'\t'<<end<<endl;
         count++;
-        end++;
+       end++;
+      
       }
-      else{
+     else if((chars.at(start)!=chars.at(end)) | end==(chars.size()-1)){
           NewChar.push_back(chars.at(start));
-          if(count!=1){
-              NewChar.push_back(to_string(count));
+         cout<<" End inside  loop  else part"<<chars[start]<<'\t'<<chars[end]<<" "<<end<<endl;
+          if(count>1){
+            // converting counting into single digit 
+            string cnt=to_string(count);
+            for(int i=0; i<cnt.length(); i++){
+              NewChar.push_back(cnt[i]);
+            }
           }
           start=end;
           count=0;
+           
       }
+      
     }
+    
     for(int i=0; i<NewChar.size(); i++){
        cout<<NewChar.at(i)<<" ";
    }
@@ -37,7 +76,7 @@ int main(){
        cin>>c;
        name.push_back(c);
    }
-   cout<<"Size of Compressed character array is : "<<CompressedStrings(name);
+   cout<<"Size of Compressed character array is : "<<CompressedString(name);
 
     return 0;
 }
