5c0415ebf69b31a2f80273dd41beadcc392decc6
Kiran Yadav <kiranyadav9551@gmail.com>
2022-07-12 22:39:23 +0530

    added traversals

--- Tree/TreeNode.cpp
+++ Tree/TreeNode.cpp
@@ -1,3 +1,8 @@
+# include <iostream>
+# include <vector>
+using namespace std;
+
+
 class TreeNode{
   int val;
   TreeNode* left;
@@ -17,3 +22,52 @@ class TreeNode{
   }
   
 };
+
+void inorderTraversalHelper(TreeNode *node, vector<int>&inorder){
+ 
+  if(node==NULL){
+  return;
+  }
+  inorderTraversalHelper(node->left, inorder);
+  inorder.push_back(nade->val);
+  inorderTraversalHelper(node->right, inorder);
+}
+
+vector<int> inorderTraversal(TreeNode* root){
+ vector<int> inorder;
+   inorderTraversalHelper(root, inrder);
+  return inorder;
+}
+
+
+void PostorderHelper(TreeNode *node, vector<int>&ans){
+ if(node==NULL){
+  return; 
+ }
+  PostorderHelper(node->left, ans);
+  PostorderHelper(node->right, ans);
+  ans.push_back(node->val);
+}
+
+vector<int>PostOrder(TreeNode *root){
+ vector<int> ans;
+  
+  PostorderHelper(root,ans);
+}
+
+
+void PreOrderHelper(TreeNode* node, vector<int>&ans){
+ if(node==NULL)
+ {
+   return;
+ }
+  ans.push_back(node->val);
+  PreOrderHelper(node->left, ans);
+  PreOrderHelper(node->right, ans);
+  
+}
+
+vector<int> PostOrder(TreeNode* root){
+ vector<int> ans;
+  PostOrderHelper(root, ans);
+}
