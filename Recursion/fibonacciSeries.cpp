e97ba094cd918747d4e4c5c0f15a5970eba91c13
Kiran Yadav <kiranyadav9551@gmail.com>
2024-06-24 23:48:03 +0530

    fivonacci series

---
+++ Recursion/fibonacciSeries.cpp
@@ -0,0 +1,16 @@
+#  include <bits/stdc++.h>
+using namespace std;
+
+void fibonacci( int secondlast, int last, int n){
+    if(n==1){
+        cout<< secondlast;
+        return ;
+    }
+    if(n==2){
+        cout<<se
+    }
+    int term=0;
+}
+int main(){
+
+}
